326b13df873972d4771c941773f687dba61d562e
ryuichi-maeda <uryu44213@gmail.com>
2023-11-27 17:33:09 +0100

    set lualine theme to vscode

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -3,7 +3,7 @@ return {
     dependencies = { "nvim-tree/nvim-web-devicons" },
     opts = {
         options = {
-            theme = "nightfox",
+            theme = "vscode",
             globalstatus = true,
         },
     },
