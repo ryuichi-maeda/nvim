8528149c77e343fa65e64fea54d02ca9c2af69ca
ryuichi-maeda <uryu44213@gmail.com>
2023-11-28 19:52:29 +0100

    refactor keymaps.lua

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -6,7 +6,6 @@ local keymap = vim.api.nvim_set_keymap
 keymap("", "<Space>", "<Nop>", opts)
 vim.g.mapleader = " "
 
-
 -- Normal --
 -- Window navigation
 keymap("n", "<C-h>", "<C-w>h", opts)
@@ -40,6 +39,7 @@ keymap("n", "<leader><S-s>", ":%s/", opts)
 keymap("v", "<", "<gv", opts)
 keymap("v", ">", ">gv", opts)
 
+-- No hlsearch
 keymap("n", "<Esc><Esc>", ":nohlsearch<Return>", opts)
 
 -- Terminal mode
@@ -57,17 +57,16 @@ keymap("n", "<leader>nt", ":Neotree toggle<Return>", opts)
 keymap("n", "<leader>nr", ":Neotree focus<Return>:execute 'normal R'<Return>", opts)
 
 -- Gitsigns
-keymap('n', '<leader>hs', ':Gitsigns stage_hunk<Return>', opts)
-keymap('v', '<leader>hs', ':Gitsigns stage_hunk<Return>', opts)
-keymap('n', '<leader>hr', ':Gitsigns reset_hunk<Return>', opts)
-keymap('v', '<leader>hr', ':Gitsigns reset_hunk<Return>', opts)
-keymap('n', '<leader>hS', ':Gitsigns stage_buffer<Return>', opts)
-keymap('n', '<leader>hu', ':Gitsigns undo_stage_hunk<Return>', opts)
-keymap('n', '<leader>hR', ':Gitsigns reset_buffer<Return>', opts)
-keymap('n', '<leader>hp', ':Gitsigns preview_hunk<Return>', opts)
-keymap('n', '<leader>hb', ':lua require"gitsigns".blame_line{full=true}<Return>', opts)
-keymap('n', '<leader>tb', ':Gitsigns toggle_current_line_blame<Return>', opts)
-keymap('n', '<leader>hd', ':Gitsigns diffthis<Return>', opts)
-keymap('n', '<leader>hD', ':lua require"gitsigns".diffthis("~")<Return>', opts)
-keymap('n', '<leader>td', ':Gitsigns toggle_deleted<Return>', opts)
-
+keymap("n", "<leader>hs", ":Gitsigns stage_hunk<Return>", opts)
+keymap("v", "<leader>hs", ":Gitsigns stage_hunk<Return>", opts)
+keymap("n", "<leader>hr", ":Gitsigns reset_hunk<Return>", opts)
+keymap("v", "<leader>hr", ":Gitsigns reset_hunk<Return>", opts)
+keymap("n", "<leader>hS", ":Gitsigns stage_buffer<Return>", opts)
+keymap("n", "<leader>hu", ":Gitsigns undo_stage_hunk<Return>", opts)
+keymap("n", "<leader>hR", ":Gitsigns reset_buffer<Return>", opts)
+keymap("n", "<leader>hp", ":Gitsigns preview_hunk<Return>", opts)
+keymap("n", "<leader>hb", ':lua require"gitsigns".blame_line{full=true}<Return>', opts)
+keymap("n", "<leader>tb", ":Gitsigns toggle_current_line_blame<Return>", opts)
+keymap("n", "<leader>hd", ":Gitsigns diffthis<Return>", opts)
+keymap("n", "<leader>hD", ':lua require"gitsigns".diffthis("~")<Return>', opts)
+keymap("n", "<leader>td", ":Gitsigns toggle_deleted<Return>", opts)
