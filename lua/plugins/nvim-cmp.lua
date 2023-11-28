8c5b27e7e71ed2d307a7e1707f5364ae6fc33afa
ryuichi-maeda <uryu44213@gmail.com>
2023-11-28 22:14:42 +0100

    add nvim-cmp plugin

---
+++ lua/plugins/nvim-cmp.lua
@@ -0,0 +1,31 @@
+return {
+	"hrsh7th/nvim-cmp",
+	event = "InsertEnter",
+	dependencies = {
+		"hrsh7th/cmp-nvim-lsp",
+		"hrsh7th/cmp-buffer",
+		"hrsh7th/cmp-path",
+	},
+	opts = function()
+		local cmp = require("cmp")
+		return {
+			completion = {},
+			mapping = cmp.mapping.preset.insert({
+				["<C-n>"] = cmp.mapping.select_next_item(),
+				["<C-p>"] = cmp.mapping.select_prev_item(),
+				["<C-e>"] = cmp.mapping.abort(),
+				["<Return>"] = cmp.mapping.confirm({ select = true }),
+			}),
+			sources = cmp.config.sources({
+				{ name = "nvim_lsp" },
+				{ name = "luasnip" },
+				{ name = "path" },
+			}, {
+				{ name = "buffer" },
+			}),
+		}
+	end,
+	config = function(_, opts)
+		require("cmp").setup(opts)
+	end,
+}
