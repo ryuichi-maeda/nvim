47c4c9fe0b1bac50fb55c3f7ddea2085c927d023
ryuichi-maeda <uryu44213@gmail.com>
2024-01-02 22:54:41 +0100

    Refactor options

--- lua/options.lua
+++ lua/options.lua
@@ -1,49 +1,48 @@
 local options = {
-    -- Basic
-    encoding = "utf-8",
-    fileencoding = "utf-8",
-    backup = false,
-    swapfile = false,
-    autoread = true,
-    hidden = true,
-    showcmd = true,
-    backspace = "indent,eol,start",
-    mouse = "a",
-    clipboard = "unnamedplus",
+	-- Basic
+	encoding = "utf-8",
+	fileencoding = "utf-8",
+	backup = false,
+	swapfile = false,
+	autoread = true,
+	hidden = true,
+	showcmd = true,
+	backspace = "indent,eol,start",
+	mouse = "a",
+	clipboard = "unnamedplus",
 
-    -- Visual
-    number = true,
-    relativenumber = true,
-    cursorline = true,
-    visualbell = true,
-    showmatch = true,
-    laststatus = 2,
-    wildmode = "list:longest",
-    wrap = false,
-    whichwrap = "b,s,h,l,<,>,[,],~",
-    background = "dark",
+	-- Visual
+	number = true,
+	relativenumber = true,
+	cursorline = true,
+	visualbell = true,
+	showmatch = true,
+	laststatus = 2,
+	wildmode = "list:longest",
+	wrap = false,
+	whichwrap = "b,s,h,l,<,>,[,],~",
+	background = "dark",
 
-    -- Indent
-    expandtab = true,
-    tabstop = 4,
-    shiftwidth = 4,
-    autoindent = true,
-    smartindent = true,
+	-- Indent
+	expandtab = true,
+	tabstop = 4,
+	shiftwidth = 4,
+	autoindent = true,
+	smartindent = true,
 
-    -- Search
-    ignorecase = true,
-    smartcase = true,
-    incsearch = true,
-    wrapscan = true,
-    hlsearch = true,
+	-- Search
+	ignorecase = true,
+	smartcase = true,
+	incsearch = true,
+	wrapscan = true,
+	hlsearch = true,
 }
 
 for k, v in pairs(options) do
-    vim.opt[k] = v
+	vim.opt[k] = v
 end
 
 -- For nvim-tree
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 vim.opt.termguicolors = true
-
